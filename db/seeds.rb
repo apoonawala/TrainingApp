2bb8729e36ba75410501248497a2bb1d6f4bd44f
arif <arif.poonawala@gmail.com>
2013-02-13 13:41:03 -0600

    AddedSeedsFile

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Athlete.create(fname: "Arif", lname: "Poonawala", email:"arif.poonawala@gmail.com", age: 29, pic_url:"https://encrypted-tbn3.gstatic.com/tbns/personal?q=tbn:ANd9GcT8y_3NkUTy4KdDupgAyWFlHsAZxsn0otwn0NbsEAyp8C3W7yFJ1hUJ2-TqqiYiS61_lTz4u-v8vhYPnmr4UKCAt1ukwrIWkd0cgVIVFEtc05ofo5LdBykzOlZx2nMnSIq65GaikHvzhSfONZs7wL6f8SdrTnxxVT-Z9Q" )
+
+Event.create(name: "Chicago Triathalon", location: "Chicago")
+
